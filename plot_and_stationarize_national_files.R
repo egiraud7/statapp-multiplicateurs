e8b040ec604e3adc9b3d1f91eb68e32228795591
VictorFrancey <113614976+VictorFrancey@users.noreply.github.com>
2023-12-07 17:22:28 +0100

    Update plot_and_stationarize_national_files.R

--- plot_and_stationarize_national_files.R
+++ plot_and_stationarize_national_files.R
@@ -5,9 +5,18 @@ library(tidyr)
 library(zoo)
 
 
-d1 <- read_excel("2023-11-07 - OECD short term indicators.xlsx")
+d_US <- read_excel("2023-12-07_BDD_clean.xlsx", sheet="US_quarter", skip=1)
+d_France <- read_excel("2023-12-07_BDD_clean.xlsx", sheet="FR_quarter", skip=1)
+d_Germany <- read_excel("2023-12-07_BDD_clean.xlsx", sheet="GER_quarter", skip=1)
 
-ts_d1 <- ts.plot(d1)
-for (i in 1:ncol(ts_d1)) {
-  ts.plot(ts_df[, i], gpars = list(main = colnames(ts_d1)[i]))
+d_US <- row.names(d_US$Quarter)
+
+ts_d_US <- ts(d_US, start =c(1990, 01), frequency = 4)
+ts.plot(ts_d_US)
+
+
+for (i in 1:ncol(ts_d_US)) {
+  ts.plot(ts_d_US[, i], gpars = list(main = colnames(ts_d_US)[i], ylab=""))
 }
+
+stationary_US <- sapply(ts_d_US, diff)
