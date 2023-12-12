f2fd6302b9ccea788010b0ebb7e8212d6061eb00
egiraud7 <152175496+egiraud7@users.noreply.github.com>
2023-12-12 15:03:02 +0100

    Update codeR_US.R

--- STatapp_github/codeR_US.R
+++ STatapp_github/codeR_US.R
@@ -1,31 +1,22 @@
-#importation donnees
-
+#Importation des donnees
 d_US <- read.xlsx("DATA_USA.xlsx", na.strings = ".", colNames=TRUE, rowNames=FALSE, detectDates = TRUE)
 
-#transfo en time serie
-
-
-
+#Transformation en time series
 ts_US = ts(d_US, start=c(1990,1), frequency=4)
 
-par(mfrow = c(2,2))
+#Affichage en 2x2 graphiques
+par(mfrow = c(2,2)) 
 
+#Creation de graphiques séparés pour l'ensemble des colonnes du dataframe avec en titre le nom des colonnes de l'Excel
 for (i in 2:ncol(ts_US)) {
   ts.plot(ts_US[, i], gpars = list(main = colnames(ts_US)[i], ylab=""))
 }
 
-
-
-#stationnary
-
-
+#Stationnarisation des séries
 ts_stationary_US <- sapply(ts_US, diff)
 
+#Creation de graphiques séparés des nouvelles séries stationnarisées avec en titre le nom des colonnes de l'Excel
 par(mfrow = c(2,2))
-
 for (i in 2:ncol(ts_stationary_US)) {
   ts.plot(ts_stationary_US[, i], gpars = list(main = paste("stationary",colnames(ts_US)[i]), ylab=""))
 }
-
-
-
